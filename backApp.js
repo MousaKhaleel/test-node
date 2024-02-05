939987ca4d4088623cdf19e1e0d61869b7d50a2f
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-02-05 02:30:24 +0200

    node first commit

---
+++ backApp.js
@@ -0,0 +1,4 @@
+var bar='tar';
+console.log('hello');
+console.log(module);
+module.exports={bar};
---
+++ other.js
@@ -0,0 +1,5 @@
+var getBar= module.require('./backApp');
+
+console.log(getBar.bar);
+var os=require('os');
+console.log(os.userInfo());
